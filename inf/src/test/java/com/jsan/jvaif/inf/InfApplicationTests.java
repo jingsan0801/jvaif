82d5d05ae88629dc371adf152589cba0e169812a
Stone <stone@mewifi.com.cn>
2019-05-28 00:30:19 +0800

    feat: 目录搭建

---
+++ inf/src/test/java/com/jsan/jvaif/inf/InfApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jsan.jvaif.inf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InfApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
