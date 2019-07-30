361e88966cc2969ec6443b876d9abe9c4139addd
jsan <jingsan0801@gmail.com>
2019-07-30 16:36:37 +0800

    feat: 引入layui后台模板

---
+++ web/src/main/java/com/jsan/jvaif/web/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.jsan.jvaif.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @description: 配置web资源映射
+ * @author: jcwang
+ * @create: 2019-07-30 16:03
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 将resources下的目录做映射
+        registry.addResourceHandler("/src/**").addResourceLocations("classpath:/src/");
+        registry.addResourceHandler("/start/**").addResourceLocations("classpath:/start/");
+    }
+}
