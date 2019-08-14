8cd6a5757debff7b83afef639c78b9cf8bfdaaf1
jsan <jingsan0801@gmail.com>
2019-08-14 22:09:31 +0800

    fit: 使用了@Configuration的RedisConfig读取不到application.properties中的值, 必须要自定义另外一个配置文件

--- inf/src/main/java/com/jsan/jvaif/inf/config/RedisConfig.java
+++ inf/src/main/java/com/jsan/jvaif/inf/config/RedisConfig.java
@@ -1,15 +1,19 @@
 package com.jsan.jvaif.inf.config;
 
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -24,7 +28,44 @@ import java.time.Duration;
  **/
 @Configuration
 @EnableCaching
+@PropertySource("classpath:config/redisConfig.properties")
 public class RedisConfig extends CachingConfigurerSupport {
+    @Value("${spring.redis.database}")
+    private int database;
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.password}")
+    private String password;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Value("${spring.redis.timeout}")
+    private long timeout;
+
+    @Value("${spring.redis.lettuce.pool.max-idle}")
+    private int maxIdle;
+
+    @Value("${spring.redis.lettuce.pool.min-idle}")
+    private int minIdle;
+
+    @Value("${spring.redis.lettuce.pool.max-active}")
+    private int maxActive;
+
+    @Value("${spring.redis.lettuce.pool.max-wait}")
+    private long maxWait;
+
+    @Bean
+    public GenericObjectPoolConfig genericObjectPoolConfig() {
+        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+        genericObjectPoolConfig.setMaxIdle(maxIdle);
+        genericObjectPoolConfig.setMinIdle(minIdle);
+        genericObjectPoolConfig.setMaxTotal(maxActive);
+        genericObjectPoolConfig.setMaxWaitMillis(maxWait);
+        return genericObjectPoolConfig;
+    }
 
     /**
      * 缓存管理器
