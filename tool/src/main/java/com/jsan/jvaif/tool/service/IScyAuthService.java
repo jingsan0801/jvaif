f4cb3340335f7d657657fdf045f4edd98175a0f3
jsan <jingsan0801@gmail.com>
2019-08-20 20:13:10 +0800

    feat: 引入代码自动生成工具

---
+++ tool/src/main/java/com/jsan/jvaif/tool/service/IScyAuthService.java
@@ -0,0 +1,16 @@
+package com.jsan.jvaif.tool.service;
+
+import com.jsan.jvaif.tool.domain.ScyAuth;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 权限表  服务类
+ * </p>
+ *
+ * @author wangjc
+ * @since 2019-08-18
+ */
+public interface IScyAuthService extends IService<ScyAuth> {
+
+}
---
+++ tool/src/main/java/com/jsan/jvaif/tool/service/impl/ScyAuthServiceImpl.java
@@ -0,0 +1,20 @@
+package com.jsan.jvaif.tool.service.impl;
+
+import com.jsan.jvaif.tool.domain.ScyAuth;
+import com.jsan.jvaif.tool.mapper.ScyAuthMapper;
+import com.jsan.jvaif.tool.service.IScyAuthService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 权限表  服务实现类
+ * </p>
+ *
+ * @author wangjc
+ * @since 2019-08-18
+ */
+@Service
+public class ScyAuthServiceImpl extends ServiceImpl<ScyAuthMapper, ScyAuth> implements IScyAuthService {
+
+}
