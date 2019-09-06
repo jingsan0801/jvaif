170ac2722d05784a1420a8a97f0223bb11fea2c2
jsan <jingsan0801@gmail.com>
2019-09-06 22:17:02 +0800

    refactor: 自定义注解消除warn

--- web/src/main/java/com/jsan/jvaif/web/aspect/HttpLogAspect.java
+++ web/src/main/java/com/jsan/jvaif/web/aspect/HttpLogAspect.java
@@ -6,13 +6,11 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Method;
 
 /**
  * @description: httpLog切面
@@ -35,17 +33,17 @@ public class HttpLogAspect {
      */
     @Before("pointCut()")
     public void before(JoinPoint joinPoint) {
-        MethodSignature methodSignature = (MethodSignature)joinPoint.getSignature();
-        Method method = methodSignature.getMethod();
         // 记录request请求参数
         ServletRequestAttributes attributes = (ServletRequestAttributes)RequestContextHolder.getRequestAttributes();
-        HttpServletRequest request = attributes.getRequest();
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
 
-        log.info("[reqMethod={}, args={}]",
-            joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(),
-            joinPoint.getArgs());
-        log.info("[reqUrl = {}, method={}, ip={}]", request.getRequestURI(), request.getMethod(),
-            request.getRemoteAddr());
+            log.info("[reqMethod={}, args={}]",
+                joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName(),
+                joinPoint.getArgs());
+            log.info("[reqUrl = {}, method={}, ip={}]", request.getRequestURI(), request.getMethod(),
+                request.getRemoteAddr());
+        }
 
     }
 
